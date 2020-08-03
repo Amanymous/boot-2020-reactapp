d98d96e5902b95527667703fcf2587dc6ad95c2f
aman mirza <aman mirza99>
2020-08-03 17:48:34 +0500

    surage react app
    yarn build
    cd build
    surage ./build
    http://bootcamp2020-reactapp-aman.surge.sh/

---
+++ build/precache-manifest.249b755d84c4b1acae462a553280ddda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9597b05be50703e706eb98c50a4f7ec9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6efefe86536a16927ca3",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "701087307c3415589d61",
+    "url": "/static/js/2.e7e8e7a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e8e7a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6efefe86536a16927ca3",
+    "url": "/static/js/main.a6260997.chunk.js"
+  },
+  {
+    "revision": "c5280eeb95843d4c7751",
+    "url": "/static/js/runtime-main.d7ce6683.js"
+  }
+]);
